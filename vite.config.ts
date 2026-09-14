6a20671ced6ad95d6ef9d9b85f62d23decf6e8d9
Ali Rahmani <alirahmanitahooneh@gmail.com>
2026-09-14 13:56:05 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { readExecutionProfile } from "./scripts/execution-profile.mjs";
 import { sites } from "./build/sites-vite-plugin";
 
 const SITE_CREATOR_PLACEHOLDER_DATABASE_ID =
-  "00000000-0000-4000-8000-000000000000";
+  "e5f2a4ab-4853-4d12-a545-17a5eab35c7c";
 
 const { d1, r2 } = hostingConfig;
 
